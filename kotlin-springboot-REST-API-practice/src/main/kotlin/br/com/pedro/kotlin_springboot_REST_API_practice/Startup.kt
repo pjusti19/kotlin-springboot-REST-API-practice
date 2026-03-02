3f0dd59aaa4caa7120c42d7e7682bf0ec6252adc
pedro.justi <pedro.justi@ifood.com.br>
2026-03-02 08:26:37 -0300

    First steps

---
+++ kotlin-springboot-REST-API-practice/src/main/kotlin/br/com/pedro/kotlin_springboot_REST_API_practice/Startup.kt
@@ -0,0 +1,11 @@
+package br.com.pedro.kotlin_springboot_REST_API_practice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Startup
+
+fun main(args: Array<String>) {
+	runApplication<Startup>(*args)
+}
---
+++ kotlin-springboot-REST-API-practice/src/test/kotlin/br/com/pedro/kotlin_springboot_REST_API_practice/StartupTests.kt
@@ -0,0 +1,13 @@
+package br.com.pedro.kotlin_springboot_REST_API_practice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
