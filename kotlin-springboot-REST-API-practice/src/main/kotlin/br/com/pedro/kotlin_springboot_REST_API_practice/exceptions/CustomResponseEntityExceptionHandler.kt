ddd60e79754b6fcb311330fb598b28a887e5fd4b
pjusti19 <pjusti19@gmail.com>
2026-03-02 13:06:01 -0300

    calculator functions endpoints

---
+++ kotlin-springboot-REST-API-practice/src/main/kotlin/br/com/pedro/kotlin_springboot_REST_API_practice/exceptions/CustomResponseEntityExceptionHandler.kt
@@ -0,0 +1,36 @@
+package br.com.pedro.kotlin_springboot_REST_API_practice.exceptions
+
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.context.request.WebRequest
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
+import java.util.Date
+
+@ControllerAdvice
+@RestController
+class CustomResponseEntityExceptionHandler : ResponseEntityExceptionHandler() {
+
+    @ExceptionHandler(Exception::class)
+    fun handleAllExceptions(ex: Exception, request: WebRequest): ResponseEntity<ExceptionResponse> {
+        val exceptionResponse = ExceptionResponse(
+            Date(),
+            ex.message,
+            request.getDescription(false)
+        )
+        return ResponseEntity<ExceptionResponse>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR)
+    }
+
+    @ExceptionHandler(UnsupportedMathOperationException::class, DivisionByZeroException::class)
+    fun handleBadRequestExceptions(ex: Exception, request: WebRequest): ResponseEntity<ExceptionResponse> {
+        val exceptionResponse = ExceptionResponse(
+            Date(),
+            ex.message,
+            request.getDescription(false)
+        )
+        return ResponseEntity<ExceptionResponse>(exceptionResponse, HttpStatus.BAD_REQUEST)
+    }
+
+}
